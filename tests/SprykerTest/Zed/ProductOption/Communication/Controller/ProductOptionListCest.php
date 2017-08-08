5607bd97efd562db469a225e06ff7abbf7d094f0
René Klatt <rene.klatt@spryker.com>
2017-08-08 11:42:37 +0000

    Moved Presentation tests to Communication tests

--- tests/SprykerTest/Zed/ProductOption/Communication/Controller/ProductOptionListCest.php
+++ tests/SprykerTest/Zed/ProductOption/Communication/Controller/ProductOptionListCest.php
@@ -5,17 +5,18 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace SprykerTest\Zed\ProductOption\Presentation;
+namespace SprykerTest\Zed\ProductOption\Communication\Controller;
 
 use SprykerTest\Zed\ProductOption\PageObject\ProductOptionListPage;
-use SprykerTest\Zed\ProductOption\ProductOptionPresentationTester;
+use SprykerTest\Zed\ProductOption\ProductOptionCommunicationTester;
 
 /**
  * Auto-generated group annotations
  * @group SprykerTest
  * @group Zed
  * @group ProductOption
- * @group Presentation
+ * @group Communication
+ * @group Controller
  * @group ProductOptionListCest
  * Add your own group annotations below this line
  */
@@ -23,15 +24,13 @@ class ProductOptionListCest
 {
 
     /**
-     * @param \SprykerTest\Zed\ProductOption\ProductOptionPresentationTester $i
+     * @param \SprykerTest\Zed\ProductOption\ProductOptionCommunicationTester $i
      *
      * @return void
      */
-    public function breadcrumbIsVisible(ProductOptionPresentationTester $i)
+    public function breadcrumbIsVisible(ProductOptionCommunicationTester $i)
     {
         $i->amOnPage(ProductOptionListPage::URL);
-        $i->wait(2);
-
         $i->seeBreadcrumbNavigation('Dashboard / Products / Product Options');
     }
 
