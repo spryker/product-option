06addd8a09617109bbdde6bb14d756c04b867a51
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Zed/ProductOption/ProductOptionConfig.php
+++ src/Spryker/Zed/ProductOption/ProductOptionConfig.php
@@ -12,16 +12,22 @@ use Spryker\Zed\Kernel\AbstractBundleConfig;
 class ProductOptionConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_TYPE_PRODUCT_OPTION = 'product_option';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const PRODUCT_OPTION_TRANSLATION_PREFIX = 'product.option.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const PRODUCT_OPTION_GROUP_NAME_TRANSLATION_PREFIX = 'product.option.group.name.';
